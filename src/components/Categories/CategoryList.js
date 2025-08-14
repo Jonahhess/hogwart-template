91b9f9927b10f378772803d9a6924094e809ad51
jonahhess <jonahhessdev@gmail.com>
2025-08-14 17:46:51 +0300

    used link to category

--- src/components/Categories/CategoryList.js
+++ src/components/Categories/CategoryList.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import CategoryCard from './CategoryCard';
+import CategoryCard from "./CategoryCard";
+import { Link } from "react-router-dom";
 
 const CategoryList = ({ categories }) => {
   return (
     <div className="category-list">
-        {/* add link to each of these */}
-        <CategoryCard id="potions" categoryData={categories[0]} />
-        <CategoryCard id="charms" categoryData={categories[1]} />
-
-      {categories.slice(2).map(category => (
-          <CategoryCard categoryData={category} />
-        ))}
+      {categories.map((category) => (
+        <Link to={`/wiki/${category.routeLink}`}>
+          <CategoryCard
+            key={category.id}
+            id={category.routeLink}
+            categoryData={category}
+          />
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
 export default CategoryList;
